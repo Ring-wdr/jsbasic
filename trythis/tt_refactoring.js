ae85cd8e1fc49b294e256f6e313ee9293d4875a4
Ring-wdr <akswnd55@gmail.com>
2022-09-24 23:00:29 +0900

    20220924 trythis start

--- trythis/tt_refactoring.js
+++ trythis/tt_refactoring.js
@@ -5,10 +5,9 @@ switch (parseInt(process.argv[2])) {
   case 1:
     const myFetch = () =>
       // new Promise((resolve, reject) =>
-      fetch("https://jsonplaceholder.typicode.com/users/1")
-        .then((res) => res.json())
-        .then(resolve)
-        .catch(reject);
+      fetch("https://jsonplaceholder.typicode.com/users/1").then((res) =>
+        res.json()
+      );
     // );
     break;
   case 2:
@@ -23,38 +22,43 @@ switch (parseInt(process.argv[2])) {
     const getAllUsers = (sql) =>
       new Promise((resolve, reject) =>
         query.execute(sql, (err, rs) => {
-          if (err) return reject(err);
-          const results = [];
-          while (rs.next()) {
-            results.push(rs.getRow());
-            !rs.next() && resolve(results);
+          try {
+            if (err) throw err;
+            const results = [];
+            while (rs.next()) {
+              results.push(rs.getRow());
+            }
+            resolve(results);
+          } catch (err2) {
+            reject(err2);
           }
         })
       );
     break;
   case 4:
-    try {
-      randTime(1)
-        // .then((res) => res)
-        .then((res) => {
-          console.log("res>>", res);
-          throw new Error("XXX");
-        })
-        .catch((err) => {
-          console.log("error!!!", err);
-          throw err;
-        });
-    } catch (Err) {
-      console.error("@@@@@@@Err>>", Err);
-    }
+    // try {
+    randTime(1)
+      // .then((res) => res)
+      .then((res) => {
+        console.log("res>>", res);
+        throw new Error("XXX");
+      })
+      .catch((err) => {
+        console.log("error!!!", err);
+        // throw err;
+      });
+    // } catch (Err) {
+    //   console.error("@@@@@@@Err>>", Err);
+    // }
     break;
   case 5:
-    promiseFn().then(
-      // 2개 인수 then
-      (res) => validFn(res), // resolve
-      handleError // reject
-    );
-    // .catch(handleError);
+    promiseFn()
+      .then(
+        // 2개 인수 then
+        (res) => validFn(res) // resolve
+        // handleError // reject
+      )
+      .catch(handleError);
 
     break;
   default:
